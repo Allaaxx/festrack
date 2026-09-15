66c1b2096a270d72271b5c27aa88e1c2553e2f4f
Allaaxx <allanrao358@gmail.com>
2026-09-15 17:15:42 -0300

    feat: implement event service and hooks with full CRUD and cache invalidation

--- src/api/hooks/event.js
+++ src/api/hooks/event.js
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import EventService from '@/api/services/event';
 import { useAuthContext } from '@/contexts/auth';
@@ -11,5 +11,53 @@ export const useGetEvents = () => {
     queryKey: getEventsQueryKey({ userId: user.id }),
     queryFn: () => EventService.getAll(),
     enabled: Boolean(user.id),
+  });
+};
+
+export const createEventMutationKey = ['createEvent'];
+
+export const useCreateEvent = () => {
+  const queryClient = useQueryClient();
+  const { user } = useAuthContext();
+  return useMutation({
+    mutationKey: createEventMutationKey,
+    mutationFn: (input) => EventService.create(input),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getEventsQueryKey({ userId: user.id }),
+      });
+    },
+  });
+};
+
+export const editEventMutationKey = ['editEvent'];
+
+export const useEditEvent = () => {
+  const queryClient = useQueryClient();
+  const { user } = useAuthContext();
+  return useMutation({
+    mutationKey: editEventMutationKey,
+    mutationFn: (input) => EventService.update(input),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getEventsQueryKey({ userId: user.id }),
+      });
+    },
+  });
+};
+
+export const deleteEventMutationKey = ['deleteEvent'];
+
+export const useDeleteEvent = () => {
+  const queryClient = useQueryClient();
+  const { user } = useAuthContext();
+  return useMutation({
+    mutationKey: deleteEventMutationKey,
+    mutationFn: (input) => EventService.delete(input),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getEventsQueryKey({ userId: user.id }),
+      });
+    },
   });
 };
